0752324159d884e93dae2eb19fba68bdc7d02fd1
Avinash Gupta <>
2024-11-19 13:09:05 +0530

    Add new Commit

--- InventoryApp/createchallanvc.swift
+++ InventoryApp/createchallanvc.swift
@@ -7,35 +7,27 @@
 
 import UIKit
 
-class createchallanvc: UIViewController {
+class createchallanvc: BaseVC  {
     
     @IBOutlet weak var Datatxt: UITextField!
     @IBOutlet weak var Nametxt: UITextField!
     @IBOutlet weak var Locationtxt: UITextField!
-    @IBOutlet weak var Remarktxt: UITextField!
+    @IBOutlet weak var txtRemarkView: UITextView!
+    
+    let placeholderText = "Enter your remarks here..."
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupDatePicker()
-        
-         setPlaceholderColor(textField: Datatxt, placeholderText: "Enter Date", color: UIColor.black)
-           setPlaceholderColor(textField: Nametxt, placeholderText: "Enter Name", color: UIColor.black)
-           setPlaceholderColor(textField: Locationtxt, placeholderText: "Enter Location", color: UIColor.black)
-           setPlaceholderColor(textField: Remarktxt, placeholderText: "Enter Remark", color: UIColor.black)
+        txtRemarkView.delegate = self
+        txtRemarkView.text = placeholderText
+        txtRemarkView.textColor = .lightGray
     }
     
-    func setPlaceholderColor(textField: UITextField, placeholderText: String, color: UIColor) {
-        textField.attributedPlaceholder = NSAttributedString(
-            string: placeholderText,
-            attributes: [NSAttributedString.Key.foregroundColor: color]
-        )
-    }
+
     
-    @IBAction func backbtn(_ sender: UIButton) {
-        self.navigationController?.popViewController(animated: true)
-    }
     
-    // MARK: - Setup Date Picker
+ // MARK: - Setup Date Picker
     private func setupDatePicker() {
         let datePicker = UIDatePicker()
         datePicker.datePickerMode = .date
@@ -43,13 +35,10 @@ class createchallanvc: UIViewController {
             datePicker.preferredDatePickerStyle = .wheels
         }
         datePicker.addTarget(self, action: #selector(dateChanged(datePicker:)), for: .valueChanged)
-        
         let toolbar = UIToolbar()
         toolbar.sizeToFit()
-        
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(donePressed))
         toolbar.setItems([doneButton], animated: true)
-        
         Datatxt.inputAccessoryView = toolbar
         Datatxt.inputView = datePicker
     }
@@ -65,48 +54,60 @@ class createchallanvc: UIViewController {
     }
     
     @IBAction func Nextbtnaction(_ sender: UIButton) {
-//        if validateFields() {
-//            let name = Nametxt.text ?? ""
-//            let date = Datatxt.text ?? ""
-//            let location = Locationtxt.text ?? ""
-//            let remark = Remarktxt.text ?? ""
-//            
-//            let challanData: [String: String] = ["name": name, "date": date, "location": location, "remark": remark]
-            
-   //         UserDefaults.standard.set(challanData, forKey: "challanData")
-            let vc = self.storyboard?.instantiateViewController(withIdentifier: "Additemvc") as! Additemvc
-            self.navigationController?.pushViewController(vc, animated: true)
-     //   }
+        if validateFields() {
+                let vc = self.storyboard?.instantiateViewController(withIdentifier: "Additemvc") as! Additemvc
+                self.navigationController?.pushViewController(vc, animated: true)
+            }
     }
     
     // MARK: - Validation
+    private func showValidationAlert(message: String) {
+        let alert = UIAlertController(title: "All Field Required", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
     private func validateFields() -> Bool {
-        var isValid = true
-        var message = ""
-        
-        if Nametxt.text?.isEmpty ?? true {
-            message += ""
-            isValid = false
+
+        // Validate Date field
+        if Datatxt.text?.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ?? true {
+            showValidationAlert(message: "Date field is required.")
+            return false
         }
-        if Datatxt.text?.isEmpty ?? true {
-            message += ""
-            isValid = false
+        // Validate Name field
+        if Nametxt.text?.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ?? true {
+            showValidationAlert(message: "Name field is required.")
+            return false
         }
-        if Locationtxt.text?.isEmpty ?? true {
-            message += ""
-            isValid = false
+        // Validate Location field
+        if Locationtxt.text?.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ?? true {
+            showValidationAlert(message: "Location field is required.")
+            return false
         }
-        if Remarktxt.text?.isEmpty ?? true {
-            message += ""
-            isValid = false
+
+        // Validate Remark TextView
+        if txtRemarkView.text?.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ?? true || txtRemarkView.text == placeholderText {
+            showValidationAlert(message: "Remarks field is required.")
+            return false
         }
-        
-        if !isValid {
-            let alert = UIAlertController(title: "Validation Error", message: "Please fill all fileds", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
+        return true
+    }
+
+
+    
+}
+// MARK: - UITextViewDelegate Methods
+extension  createchallanvc : UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == placeholderText {
+            textView.text = ""
+            textView.textColor = .black
         }
-        
-        return isValid
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = placeholderText
+            textView.textColor = .lightGray
+        }
     }
 }
